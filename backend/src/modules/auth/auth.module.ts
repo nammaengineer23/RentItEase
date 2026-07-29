6d35dd113ace2c8936305b5100bfc630097ce8f7
nammaengineer23 <nammaengineer23@gmail.com>
2026-07-29 10:32:14 +0530

    Fix NestJS production build and complete Flutter UI modules

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -8,6 +8,7 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { MailModule } from '../../mail/mail.module';
+import { OtpModule } from '../../common/otp/otp.module';
 
 
 @Module({
@@ -15,6 +16,7 @@ import { MailModule } from '../../mail/mail.module';
     ConfigModule,
     FirebaseModule,
     MailModule,
+    OtpModule,
     PassportModule.register({
       defaultStrategy: 'jwt',
     }),
