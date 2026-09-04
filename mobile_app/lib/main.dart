1a968af8f1cda36913f8090c92d1a04fb26fa222
RentItEase <rentitease@gmail.com>
2026-09-04 13:48:37 +0530

    chore(mobile): synchronize FlutterFire lockfile

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -10,9 +10,7 @@ import 'firebase_options.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   await FirebaseAppCheck.instance.activate(
     providerAndroid: kDebugMode
