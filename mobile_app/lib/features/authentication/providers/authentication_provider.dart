95853e35f4dfa9ddd30a12306b246bf7d607fc59
nammaengineer23 <rentitease@gmail.com>
2026-09-12 09:46:15 +0530

    fix: remove redundant auth provider import

--- mobile_app/lib/features/authentication/providers/authentication_provider.dart
+++ mobile_app/lib/features/authentication/providers/authentication_provider.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import '../data/models/auth_response.dart';
