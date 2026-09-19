3e3e6c6e1b7504d92994d84444cd58434f1723c3
nammaengineer23 <rentitease@gmail.com>
2026-09-19 10:56:20 +0530

    chore(web): scope browser visibility analyzer exemptions

--- mobile_app/lib/core/platform/web_visibility_web.dart
+++ mobile_app/lib/core/platform/web_visibility_web.dart
@@ -1,4 +1,5 @@
-// ignore: deprecated_member_use
+// ignore_for_file: avoid_web_libraries_in_flutter, deprecated_member_use
+
 import 'dart:html' as html;
 
 import 'package:flutter/foundation.dart';
