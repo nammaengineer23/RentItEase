41424ee01703be406229b5e96f211017ae6624b4
nammaengineer23 <rentitease@gmail.com>
2026-09-22 13:36:27 +0530

    feat: add persisted social reel preview migration

---
+++ backend/prisma/migrations/20260922120000_add_social_reel_preview/migration.sql
@@ -0,0 +1,5 @@
+ALTER TABLE "SocialMarketingConsent"
+ADD COLUMN "preparedVideoUrl" TEXT,
+ADD COLUMN "preparedCaption" TEXT,
+ADD COLUMN "preparedTitle" TEXT,
+ADD COLUMN "preparedAt" TIMESTAMP(3);
