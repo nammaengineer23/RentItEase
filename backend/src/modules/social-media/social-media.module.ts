9b3fbae90fd99dfd70e777c6d99634b2aa1ca43c
nammaengineer23 <rentitease@gmail.com>
2026-09-24 10:09:51 +0530

    refactor: use Remotion as social reel renderer

--- backend/src/modules/social-media/social-media.module.ts
+++ backend/src/modules/social-media/social-media.module.ts
@@ -9,8 +9,6 @@ import { InstagramService } from './publishing/instagram.service';
 import { PublishingService } from './publishing/publishing.service';
 import { YouTubeService } from './publishing/youtube.service';
 import { SocialMediaProcessor } from './processors/social-media.processor';
-import { VideoGeneratorService } from './video/video-generator.service';
-import { VideoService } from './video/video.service';
 import { VideoTemplateService } from './video/video-template.service';
 import { RemotionVideoService } from './video/remotion-video.service';
 import { SocialMediaStorageService } from './social-media.storage.service';
@@ -22,8 +20,6 @@ import { CampaignSchedulerService } from './campaigns/campaign-scheduler.service
   providers: [
     SocialMediaService,
     SocialMediaProcessor,
-    VideoService,
-    VideoGeneratorService,
     RemotionVideoService,
     VideoTemplateService,
     PublishingService,
